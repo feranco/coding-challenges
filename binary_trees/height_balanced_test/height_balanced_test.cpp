2290a7c7676c1f89e057af01f65ab29517e4bab2
feranco <feranco@gmail.com>
2017-07-26 23:50:05 +0200

    added new folder

---
+++ binary_trees/height_balanced_test/height_balanced_test.cpp
@@ -0,0 +1,13 @@
+//algorithm to test if a binary tree is height-balanced
+//assumptions:
+//1) height balanced = height of left and right subtrees differs at most 1
+//O(n) time, O(n) space
+
+#include <iostream>
+#include "../../binary_trees/common/defs.h"
+
+using std::unique_ptr;
+
+bool isHeightBalanced (const unique_ptr<BtNode<int>>& root) {
+
+}
