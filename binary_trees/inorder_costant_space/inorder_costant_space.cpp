10932b0240627cafef2168a01db43f43be7cc43d
feranco <feranco@gmail.com>
2017-08-03 10:26:20 +0200

    improved inorder traversal in costant space

--- binary_trees/inorder_costant_space/inorder_costant_space.cpp
+++ binary_trees/inorder_costant_space/inorder_costant_space.cpp
@@ -3,6 +3,7 @@
 //Assumptions:
 //1) tree nodes have pointers to parents
 
+#include <algorithm>
 #include <iostream>
 #include <unordered_map>
 #include <vector>
@@ -12,37 +13,39 @@ using std::cout;
 using std::unique_ptr;
 using std::vector;
 
+enum class State {DOWN, UP, EXIT};
+
 vector<char> inorderTraversal (const unique_ptr<BtNode<char>>& root) {
   vector<char> traversal;
   auto iter = root.get();
+  State state = State::DOWN;
     
-  while (true) {// go down
-    while (iter->left) iter = iter->left.get();
-    traversal.push_back(iter->data);
-    if (iter->right) {
-      iter = iter->right.get();
-      continue;
+  while (state != State::EXIT) {// go down
+
+    if (state == State::DOWN) {
+      while (iter->left) iter = iter->left.get();
+      traversal.push_back(iter->data);
+      if (iter->right) iter = iter->right.get();
+      else state = State::UP;
     }
-    else {//go up
+
+    if (state == State::UP) {//go up
       auto child = iter;
       iter = iter->parent;
-      while (true) {
 	
-	if (iter->left.get() == child) {
-	  traversal.push_back(iter->data);
-	  if (iter->right.get()) {iter = iter->right.get(); break;}
-	}
-                
-	if (iter->right.get() == child && iter == root.get()) return traversal;
-                
-	child = iter;
-	iter = iter->parent;           
+      if (iter->left.get() == child) {
+	traversal.push_back(iter->data);
+	if (iter->right.get()) {iter = iter->right.get(); state = State::DOWN; continue;}
       }
+                
+      if (iter->right.get() == child && iter == root.get()) state = State::EXIT;                        
     }
   }
+  return traversal;
 }
 
 int main (void) {
   auto tree = buildCharBtree();
   auto inorder = inorderTraversal(tree);
+  std::for_each(inorder.begin(),inorder.end(),[](const char& n){cout << n << "\n";});
 }
