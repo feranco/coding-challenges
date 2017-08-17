f92b400cbc2d44d81d616f4d26a02b098426be4e
feranco <feranco@gmail.com>
2017-08-17 22:21:40 +0200

    correct group by value

--- sorting/group_students_by_age/group_students_by_age.cpp
+++ sorting/group_students_by_age/group_students_by_age.cpp
@@ -1,6 +1,9 @@
+#include <iostream>
 #include <string>
 #include <vector>
 #include <unordered_map>
+
+using namespace std;
 
 class Student {
   int age;
@@ -10,25 +13,24 @@ public:
     this->age = age;
     this->name = name;
   }
-  int age (void) {return age;}
+  int getAge (void) {return age;}
 };
 
 void groupByAge (vector<Student>* students_ptr) {
   vector<Student>& students =  *students_ptr;
 
   unordered_map<int,int> age_frequency;
-  for (auto student : students) ++age_frequency[student.age()];
+  for (const auto& student : students) ++age_frequency[student.getAge()];
 
   unordered_map<int,int> age_offset;
   int offset = 0;
-  for (auto frequency : age_frequency) {
-    age_offset.insert({frequency->first,offset});
-    offset += frequency->second;
+  for (const auto& frequency : age_frequency) {
+    age_offset.insert({frequency.first,offset});
+    offset += frequency.second;
   }
 
-  int idx = 0;
   while (!age_offset.empty()) {
-    auto got = age_offset.find(students[idx].age());
+    auto got = age_offset.begin();
     if (got != age_offset.end()) {
       swap(students[got->second],students[idx]);
       --age_frequency[got->first];
@@ -38,4 +40,10 @@ void groupByAge (vector<Student>* students_ptr) {
   }
 }
 
+int main (void) {
+  vector<Student> students = {{"Pippo",14},{"Pluto",12},{"Topolino",11},
+			      {"Luca",13},{"Tim",12},{"Bob",13},{"Tom",13},{"Afrim",14}};
+  groupByAge(&students);
+  for (auto student : students) cout << student.getAge();
+}
 
