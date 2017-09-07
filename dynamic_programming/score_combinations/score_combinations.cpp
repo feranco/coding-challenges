a9c4e6bb715644a3b67adc7e19585f65890c12fa
feranco <feranco@gmail.com>
2017-09-07 23:06:26 +0200

    completed score combinations (dp)

--- dynamic_programming/score_combinations/score_combinations.cpp
+++ dynamic_programming/score_combinations/score_combinations.cpp
@@ -1,31 +1,60 @@
 //algorithm to count the possible point combinations to get a score
 
-#include <array>
+#include <algorithm>
 #include <iostream>
-#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
-int scoreCombinations (const array<int,3>& points, int score, unordered_map<int,int>* score_to_combinations_ptr) {
-  //base case
-  if (score < points[0]) return 0;
-  unordered_map<int,int>& score_to_combinations = *score_to_combinations_ptr;
-  if (score_to_combinations.find(score) == score_to_combinations.end()) {
-    score_to_combinations[score] =
-      scoreCombinations(points, score-points[0], score_to_combinations_ptr) +
-      scoreCombinations(points, score-points[1], score_to_combinations_ptr) +
-      scoreCombinations(points, score-points[2], score_to_combinations_ptr);
+//Solution:
+//dp with table (bottom up): use a recurrence formula
+//computing each result using the previous ones
+//recurrence: table[i][j] = table[i-1][j] + table[i-1][j-plays[i]] + table[i-1][j-2*plays[i]] 
+//table[0][0] = 1 otherwise
+//O(n^2)
+#if 0
+int scoreCombinations(const vector<int>& plays, int score) {
+
+  int combinations = 0;
+  vector<vector<int>> table(plays.size(), vector<int>(score+1, 0));
+
+  //fill the table when only 1 plays is used
+  for (int i = 0; i < table[0].size(); i += plays[0]) table[0][i] = 1;
+
+  for (int i = 1; i < plays.size(); ++i) {
+    for (int j = 0; j < score; ++j) {
+      for (int k = 0; (j - k*plays[i]) >= 0; ++k) {
+	table[i][j] += table[i - 1][j - k*plays[i]];
+      }
+    }
   }
-  return score_to_combinations[score];
+  return table[plays.size()-1][score];
+}
+#endif
+
+//O(s) space
+int scoreCombinationsOpt(const vector<int>& plays, int score) {
+
+  int combinations = 0;
+  vector<int> table(score+1, 0);
+
+  //fill the table when only 1 plays is used
+  for (int i = 0; i < table.size(); i += plays[0]) table[i] = 1;
+
+  for (int i = 1; i < plays.size(); ++i) {
+    for (int j = score; j >= 0; --j) {
+      int res = 0;
+      for (int k = 0; (j - k*plays[i]) >= 0; ++k) {
+	res += table[j - k*plays[i]];
+      }
+      table[j] = res;
+    }
+  }
+  return table.back();
 }
 
-int scoreCombinations (const array<int,3>& points, int score) {
-  unordered_map<int,int> score_to_combinations;
-  for (int point : points) score_to_combinations[point] = 1;
-  return scoreCombinations (points, score, &score_to_combinations);
-}
 
-int main (void) {
-  array<int,3> points{{2,3,7}};
-  cout << scoreCombinations(points,12);
+int main(void) {
+  vector<int> plays = { 2,3,7 };
+  cout << scoreCombinationsOpt(plays, 12);
 }
